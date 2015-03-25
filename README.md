ba14e2810555093d99bdbc3ef02b98c9372f2ea6
Shakeel Mohamed <smohamed@splunk.com>
2015-03-24 22:57:18 -0700

    Make it work for all playlists

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Don't Watch Later
-A simple Google Chrome extension to disable autoplay for videos from your YouTube "Watch Later" playlist.
+A simple Google Chrome extension to disable autoplay for videos from your YouTube playlists, most helpful for the watch later playlist.
 
-YouTube URLs in the form `https://www.youtube.com/watch?v=XXXXXXXXXXXX&index=123&list=WL` will attempt to be rewritten as `https://www.youtube.com/watch?v=XXXXXXXXXXXX` to prevent YouTube's autoplay from being triggered.
+YouTube URLs in the form 
 
-This is also an exercise in creating a minimalist Google Chrome extension.
+`https://www.youtube.com/watch?v=XXXXXXXXXXXX&index=123&list=XYZ`
+
+will attempt to be rewritten as
+
+`https://www.youtube.com/watch?v=XXXXXXXXXXXX&index=123`
+
+to prevent YouTube's autoplay from being triggered.
+
+## Why did you do this?
+
+* I love YouTube, and I hate autoplay
+* I wanted to make a minimalist Google Chrome extension
+
+I usually open videos in a new tab from my Watch Later playlist, which automatically has the `list=WL` querystring parameter. I also don't usually watch videos in the order I added them to my Watch Later playlist. Instead of trying to hack YouTube's massive amounts of JavaScript, this plugin does exactly what I need.
+
+# Contact
+
+Find me on Twitter: [@_Shakeel](http://twitter.com/_shakeel)
+
+# License
+
+[MIT](LICENSE)
--- background.js
+++ background.js
@@ -1,13 +1,48 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    if (typeof details && details.hasOwnProperty("url")) {
-        if (details.url.indexOf("&list=WL") !== -1) {
-            return {redirectUrl: details.url.replace("&list=WL", "")};
+/**
+ * For laziness' sake, copy/pasted this util function
+ * from http://stackoverflow.com/a/1634841/2785681
+ */
+
+function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');   
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var parts = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = parts.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (parts[i].lastIndexOf(prefix, 0) !== -1) {  
+                parts.splice(i, 1);
+            }
         }
+
+        url = urlparts[0] + '?' + parts.join('&');
+        return url;
     }
-    return details;
-},
-{
-    urls : ["*://*.youtube.com/*"]
-}, 
-["blocking"]
+    else {
+        return url;
+    }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if (typeof details && details.hasOwnProperty("url")) {
+            if (details.url.indexOf("&list=") !== -1) {
+                /**
+                 * Alternatively, for just the Watch Later playlist, use the following:
+                 *
+                 * return {redirectUrl: details.url.replace("&list=WL", "")};
+                 */
+                return {redirectUrl: removeURLParameter(details.url, "list")};
+            }
+        }
+        return details;
+    },
+    {
+        urls : ["*://*.youtube.com/*"]
+    },
+    ["blocking"]
 );
